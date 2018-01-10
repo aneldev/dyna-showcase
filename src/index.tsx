c372a1aeafaee76c74e8f7f06f6f0eb476f56e79
Dennis Teo <dennis@anel.co>
2018-01-10 08:01:07 +0100

    dyna-guid bump

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import {faIcon} from './utils/faIcon';
 const menuStyleWhite = require('../styles/menu-style-white.module.less');
 const menuStyleRed = require('../styles/menu-style-red.module.less');
 
-import {IShowcase, IShowcaseView, IShowcaseViewProps } from './interfaces';
+import {IShowcase, IShowcaseView, IShowcaseViewProps} from './interfaces';
 
 import "font-awesome/css/font-awesome.css";
 import "./index.less";
 
-export{
-  IShowcase, IShowcaseView, IShowcaseViewProps,
-  DynaShowcase,
-  faIcon,
-  menuStyleWhite, menuStyleRed,
+export {
+	IShowcase, IShowcaseView, IShowcaseViewProps,
+	DynaShowcase,
+	faIcon,
+	menuStyleWhite, menuStyleRed,
 }
